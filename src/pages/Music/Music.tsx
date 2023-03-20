01067074319045910a9d6dacf4d781104558d6e4
alexeev danil <dani.3098@mail.ru>
2023-03-20 15:46:59 +0500

    try to make paggination

--- src/pages/Music/Music.tsx
+++ src/pages/Music/Music.tsx
@@ -1,13 +1,20 @@
 import { useAppDispatch, useAppSelector } from 'hooks';
 import { useEffect } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
 import { musicGetService } from 'services/music/musicGetService';
 import { getMusicTracks } from 'store/slices/musicSlice/musicSlice';
-import { MusicCard } from 'ui/components/kit';
+import { Button, Input, InputType, MusicCard } from 'ui/components/kit';
 import cls from './style.module.scss';
 
 const Music = () => {
     const tracks = useAppSelector(getMusicTracks);
     const dispatch = useAppDispatch();
+    const countCards = 10;
+    const countPages = Math.ceil(tracks.length / countCards);
+    const currentPage = 2 ;
+    const paggingArray = Array.from(Array(countPages+1).keys()).slice(1);
+    const musicPage = tracks.slice(countCards * (currentPage - 1), countCards * currentPage);
+    console.log(paggingArray.values);
 
     useEffect(() => {
         dispatch(musicGetService());
@@ -15,8 +22,17 @@ const Music = () => {
 
     return (
         <div className={cls.music}>
+            <div className={cls.inputContainer}>
+                <AiOutlineSearch className={cls.logo} />
+                <Input
+                    className={cls.search}
+                    placeholder="search"
+                    inputType={InputType.BOTTOM_LINE}
+                />
+            </div>
+
             <div className={cls.cardWrapper}>
-                {tracks.map((track) => (
+                {musicPage.map((track) => (
                     <MusicCard
                         key={track.id}
                         pathImg={track.imagePath}
@@ -24,6 +40,15 @@ const Music = () => {
                         artistName={track.artist.name}
                     />
                 ))}
+            </div>
+            <div className={cls.paginationButtons}>
+                {paggingArray.map((page) => (
+                    <Button className={cls.button}
+                        key={page}
+                    >
+                        {page}</Button>
+                ))}
+
             </div>
         </div>
     );
